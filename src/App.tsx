07886af215df86b38971da69ade542fde904310a
bearddude1 <baapteshuva@gmail.com>
2022-03-30 18:29:04 +0300

    added count

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import store from './store/store';
-import { Button } from 'react-bootstrap';
-import { addNoteAction } from './store/actions/noteActions';
-import Cards from './components/Cards';
+import store from "./store/store";
+import { Button } from "react-bootstrap";
+import { addNoteAction } from "./store/actions/noteActions";
+import Cards from "./components/Cards";
+import { useState } from "react";
 
 function App() {
-  const handleClick = (e:any) => {
-    e.preventDefault()
+  const handleClick = (e: any) => {
+    e.preventDefault();
     store.dispatch({
       type: addNoteAction,
       payload: {
@@ -14,12 +15,15 @@ function App() {
         task: "Do Something " + Math.floor(Math.random() * 100),
       },
     });
-  }
-  
+  };
+  const [count, setCount] = useState(store.getState().notes.length);
+  store.subscribe(() => setCount(store.getState().notes.length));
+
   return (
     <div>
-      <Button onClick={(e)=>handleClick(e)}>Add note</Button>
-      <Cards/>
+      {count > 0 ? "You have " + count + " task to get done " : null}
+      <Button onClick={(e) => handleClick(e)}>Add a Task</Button>
+      <Cards />
     </div>
   );
 }
